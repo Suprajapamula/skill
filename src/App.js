c8660d960fbcb868782cfc85a2779eb9046f51a8
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-17 20:52:29 +0530

    signUpForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[data,setData]=useState({
+      username:"",
+      email:"",
+      password:"",
+      confirmPassword:"",
+  })
+  function handleChange(e){
+    setData({...data,[e.target.name]:e.target.value})
+  }
+  // const handleSubmit=e=>{
+  //   e.preventDefault()
+  //   console.log(data)
+  // }
+  function handleSubmit(e){
+    e.preventDefault();
+    if(password===confirmPassword){
+      console.log(data)
+    }
+    else{
+      console.log("Passwords are not matched");
+    }
+  }
+  const{username,email,password,confirmPassword}=data;
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+      <center>
+          <input type="text" name="username" value={username} placeholder='username' onChange={handleChange}></input><br/>
+          <input type="email" name="email" value={email} placeholder='email' onChange={handleChange}></input><br/>
+          <input type="password" name="password" value={password} placeholder='password' onChange={handleChange}></input><br/>
+          <input type="password" name="confirmPassword" value={confirmPassword} placeholder='confirm password' onChange={handleChange}></input><br/>
+          <button type='submit'>submit</button>
+      </center>
+      </form>
     </div>
-  );
+  )
+  
 }
 
 export default App;
