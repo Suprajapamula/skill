48101bbdb248ba342bb6ceb6206b519465b86a71
Suprajapamula <suprajapamula1449@gmail.com>
2024-01-17 12:17:39 +0530

    useEffectWithEmptyDependency

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+  const[count,setCount]=useState(0);
+  useEffect(()=>{console.log("clicked",count)},[])//Empty dependency, It will executes after return statement for only once
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <center>
+          <h1>You clicked for {count} times</h1>
+          <button onClick={()=>setCount(count+1)}>click</button>
+       </center>
     </div>
   );
 }
