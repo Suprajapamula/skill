38df87b61f5140aa2da7d4bd048cf21abf79d857
Suprajapamula <suprajapamula1449@gmail.com>
2024-01-17 12:27:43 +0530

    onChangeEventHandler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+  const[user,setUser]=useState("");
+  //function handle(e){
+  //   setUser(e.target.value)
+  //}
+  const handle=e=>{setUser(e.target.value)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <input type="text" placeholder='Username' value={user} name='user' onChange={handle}/>
+        <h1>{user}</h1>
+      </center>
     </div>
   );
 }
