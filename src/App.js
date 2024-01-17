37af51a2e9fe3cafe130641ae416ddec4655b4d9
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-17 16:26:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[data,setData]=useState({
+    username:'',
+    password:'',
+  })
+  let {username,password}=data;
+  function handleOnChnage(e){
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+  function handleOnSubmit(e){
+    e.preventDefault();
+    console.log(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <center>
+          <input type="text" placeholder='username' value={username} name="username" onChange={handleOnChnage}></input><br/>
+          <input type="password" placeholder='password' value={password} name="password" onChange={handleOnChnage}></input><br/>
+          <input type="submit"></input>
+        </center> 
+      </form>
     </div>
   );
 }
