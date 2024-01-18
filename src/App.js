457685bd18aafdd003b90b49152473beb1cccea5
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-18 11:29:35 +0530

    fetchMethod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React,{useEffect, useState} from 'react'
+
+function App(){
+   const[data,setData]=useState([])
+      useEffect(()=>{fetch("https://jsonplaceholder.typicode.com/todos").then((res)=>res.json().then((res)=>{
+        setData(res)
+      }))},[])
+ return(
+  <div>
+    <center>
+         <h1>Data</h1>
+    </center>
+         <ul>
+         {
+          data.map((item)=>{
+            return <li key={item.id}>{item.title}</li>
+          })
+         }
+         {console.log(data)}
+         </ul>
+  </div>
+ )  
 }
 
 export default App;
