845e254e86f0a91ce90c2272556cc31352d3aa94
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-19 10:25:46 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { Link,Outlet } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+        <ul>
+            <Link to="/"><li><h1>Home</h1></li></Link>
+            <Link to="/dashboard"><li><h1>Dashboard</h1></li></Link>
+            <Link to="/about"><li><h1>About</h1></li></Link>
+        </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
