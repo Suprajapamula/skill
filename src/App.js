5206aac2592c913673d611b089976cae0aaecd2e
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-19 15:58:05 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import Image from './reactlogo.jpeg';
 import Logo from './logo.svg';
+import Music from './music.mp3';
 function App() {
   return (
     <div style={{backgroundColor:"black", border:"1px solid black", textAlign:"center"}}>
      <h1 style={{color:"white"}}>Hello</h1>
      <img src={Logo} width={500} height={500}/>
      <img src={Image} width={200} height={200}/>
+     <hr/>
+     <audio controls>
+        <source src={Music} type='audio/ogg'/>
+      </audio>
     </div>
   );
 }
