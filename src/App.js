e4781541c8453d16822eae20a9b2864223cf5572
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-20 21:50:18 +0530

    Downloading files using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Logo from './logo.svg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <h1>Downloading the file using React...</h1>
+       <img src={Logo} alt="image" height="250" width="300"/>
+       <a href={Logo} download="image"><button>Click heare to download</button></a>
+       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
