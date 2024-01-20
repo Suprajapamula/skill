8a06522b8377f3d40a934ccd3a18ecaeaa2566dd
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-20 22:35:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Cities from './cities.json'
 function App() {
+  const[search,setSearch]=useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h1>search your city in Telangana</h1>
+      <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        {
+        Cities.filter((city)=>city.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map((city)=>{
+           return <div style={{border:"1px solid black", margin:"10px", padding:"10px", maxWidth:"70%"}}>{city.name}</div>
+        })
+      }
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
