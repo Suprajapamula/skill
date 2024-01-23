53e3b82f840bff6435fddc8352a011689e08c3f9
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-01-23 23:26:29 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 function App() {
+  const data=useRef();
+  useEffect(()=>{
+    data.current.focus()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+      <center>
+        <h1>Hello world</h1>
+        <form onSubmit={
+          (e)=>{e.preventDefault();
+                console.log(data.current.value);
+          }
+        }>
+        <input type="text" ref={data}/>
+        <button>Submit</button>
+        </form>
+        </center>
+
+     </div>
+  )
 }
 
-export default App;
+export default App
