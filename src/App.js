4101eb8b08c1aaeea46ec260834300c2de00c9d2
Suprajapamula <145562215+Suprajapamula@users.noreply.github.com>
2024-02-04 15:52:28 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
+  const[city,setCity]=useState("");
+  const [res,setRes]=useState("");
+  const handleChange=e=>{
+      setCity(e.target.value);
+  }
+  const handleSubmit=e=>{
+    e.preventDefault()
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then((res)=>{
+      setRes(`Temperature at ${city} is`+" "+Math.round(res.data.main.temp-273.15))
+      setCity("")
+    })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <center>
+        <div style={{boxShadow:"10px 10px 20px 0px rgba(0, 0, 0, 0.5)", margin:"100px", marginTop:"100px"}}>
+          <h1>Weather Application</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleChange} value={city}/><br/>
+            <input type="submit" value="Get Temperature" style={{backgroundColor:"lightblue"}}/>
+          </form>
+          <h1>{res}</h1>
+        </div>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
